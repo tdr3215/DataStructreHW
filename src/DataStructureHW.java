5d70fcb95093df7f8e7e5395d373f2e5dd7053ea
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-20 16:52:17 -0400

    first commit

---
+++ src/DataStructureHW.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class DataStructureHW {
+
+	String localVar = "Hi! I'm a local!";
+
+	public static void main(String[] args) {
+//		double double --> Invalid Variable declaratorId
+		double dub = 129348930.02034;
+		System.out.println(dub); // prints 1.2934893002034E8
+
+		String name = "Hello World";
+		System.out.println(name); // prints "Hello World"
+
+//		Integer 1 = 4; // the left side of the argument must be a variable
+		Integer one = 1;
+		System.out.println(one);
+
+		double TAX_RATE = 7.0;
+		System.out.println(TAX_RATE);
+		
+		String localVar = "I'm no longer local";
+		System.out.println(localVar);
+
+	}
+
+
+
+}
